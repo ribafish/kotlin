138f6fb293c9e25965f805aec64dd2eaa4c6cd05
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-10-12 17:07:56 +0200

    [K/JS] Remove extra indent inside the lowering description

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/JsLoweringPhases.kt
@@ -51,7 +51,7 @@ private val collectClassDefaultConstructorsPhase = makeIrModulePhase(
 private val prepareCollectionsToExportLowering = makeIrModulePhase(
     ::PrepareCollectionsToExportLowering,
     name = "PrepareCollectionsToExportLowering",
-    description = "Add @JsImplicitExport to exportable collections all the  declarations which we don't want to export such as `Enum.entries` or `DataClass::componentN`",
+    description = "Add @JsImplicitExport to exportable collections all the declarations which we don't want to export such as `Enum.entries` or `DataClass::componentN`",
 )
 
 private val preventExportOfSyntheticDeclarationsLowering = makeIrModulePhase(
