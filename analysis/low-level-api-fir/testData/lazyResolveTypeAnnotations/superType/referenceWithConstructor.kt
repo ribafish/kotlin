9c7f55eeb3b273815c6e15b9904623e7058b1ce2
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-11-03 22:40:44 +0100

    [LL FIR] introduce lazy resolve tests for type annotations
    From one side to check how annotation propagation works and from another
    side to cover scenarios with lazy resolution from type position as it is
    a valid case of usage in Analysis API (KtFirAnnotationListForType)
    
    ^KT-63042

---
+++ analysis/low-level-api-fir/testData/lazyResolveTypeAnnotations/superType/referenceWithConstructor.kt
@@ -0,0 +1,10 @@
+package util
+
+@Target(AnnotationTarget.TYPE)
+annotation class Anno(val position: String)
+
+const val prop = "str"
+
+interface I
+
+class <caret>MyClass() : @Anno("super type ref $prop") List<@Anno("nested super type ref $prop") List<@Anno("nested nested super type ref $prop") I>>
