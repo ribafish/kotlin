1d1e7d44e185e8e0b5d0bc57b038cf66d02ad8c6
Igor Chevdar <igor.chevdar@jetbrains.com>
2023-11-21 18:42:22 +0200

    fixup! [K/N][tests] Added some tests on generic safe casts

---
+++ kotlin-native/backend.native/tests/codegen/basics/unchecked_cast7.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+package codegen.basics.unchecked_cast7
+
+import kotlin.test.*
+
+@Test
+fun runTest() {
+    try {
+        val x = Any().uncheckedCast<Int?>()
+        println(x)
+    } catch (e: ClassCastException) {
+        println("Ok")
+    }
+}
+
+fun <T> Any?.uncheckedCast(): T = this as T
