4884935198d91f3c565871ac8cbc5889578e0e8a
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2023-12-04 11:18:22 +0100

    [Gradle] Add integration tests for building through xcode with embedAndSign task
    Including checks:
    * running embedAndSign through a build phase
    * running embedAndSign through a scheme pre-action script
    * running embedAndSign through a scheme pre-action script and using Kotlin framework in local SPM package
    * absense of explicit '-framework' linker flag
    
    ^KT-62376
    ^KT-63819

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/xcodeDirectIntegration/shared/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+}
+
+repositories {
+    mavenLocal()
+}
