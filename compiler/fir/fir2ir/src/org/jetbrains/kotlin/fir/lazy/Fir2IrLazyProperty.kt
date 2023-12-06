0e182d88815e82c5839da16c8215e2ef88b8acfb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2023-12-06 12:10:48 +0200

    [FIR2IR] Properly calculate overridden symbols for lazy declarations
    This change affects only mode with IR f/o generator
    
    The old way of overridden computation with fir2ir f/o generator relied
      on the fact that fir2ir generator creates IR for all f/o and fills
      its caches. But with IR f/o generator enabled, we don't call fir2ir
      generator, so some caches are missing. And for this mode it's enough
      to acquire the symbol using the original declaration symbol and the
      lookup tag of the corresponding supertype

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazyProperty.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazyProperty.kt
@@ -214,17 +214,34 @@ class Fir2IrLazyProperty(
     }
 
     override var overriddenSymbols: List<IrPropertySymbol> by lazyVar(lock) {
-        if (containingClass == null) return@lazyVar emptyList()
+        when (configuration.useIrFakeOverrideBuilder) {
+            true -> computeOverriddenSymbolsForIrFakeOverrideGenerator()
+            false -> computeOverriddenUsingFir2IrFakeOverrideGenerator()
+        }
+    }
+
+    // TODO: drop this function after migration to IR f/o generator will be complete
+    private fun computeOverriddenUsingFir2IrFakeOverrideGenerator(): List<IrPropertySymbol> {
+        if (containingClass == null) return emptyList()
         if (isFakeOverride && parent is Fir2IrLazyClass) {
             fakeOverrideGenerator.calcBaseSymbolsForFakeOverrideProperty(
                 containingClass, this, fir.symbol
             )
             fakeOverrideGenerator.getOverriddenSymbolsForFakeOverride(this)?.let {
                 assert(!it.contains(symbol)) { "Cannot add function $symbol to its own overriddenSymbols" }
-                return@lazyVar it
+                return it
             }
         }
-        fir.generateOverriddenPropertySymbols(containingClass)
+        return fir.generateOverriddenPropertySymbols(containingClass)
+    }
+
+    private fun computeOverriddenSymbolsForIrFakeOverrideGenerator(): List<IrPropertySymbol> {
+        if (containingClass == null || parent !is Fir2IrLazyClass) return emptyList()
+        val baseFunctionWithDispatchReceiverTag =
+            fakeOverrideGenerator.computeBaseSymbolsWithContainingClass(containingClass, fir.symbol)
+        return baseFunctionWithDispatchReceiverTag.map { (symbol, dispatchReceiverLookupTag) ->
+            declarationStorage.getIrPropertySymbol(symbol, dispatchReceiverLookupTag) as IrPropertySymbol
+        }
     }
 
     override var metadata: MetadataSource?
